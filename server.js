d57cba8fa414051afed2a26cbea1f92b8657188f
Fr0nny <fronnythedev@gmail.com>
2018-05-08 19:31:01 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signIn = require('./controllers/signIn');
+const profileID = require('./controllers/profileID');
+const image = require('./controllers/image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : '',
+    database : 'smart-brain'
+  }
+});
+
+db.select('*').from('users').then(data => {
+	// console.log(data);
+});
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+app.get('/', (req, res)=>{
+	res.send(database.users);
+})
+
+//signin --> POST = success/fail
+app.post('/signIn', (req, res) => { signIn.handleSignIn(req, res, db, bcrypt) });
+//shorter way:
+// app.post('/signIn', signIn.handleSignIn(db, bcrypt)); <--------------
+
+//register --> POST = user
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
+
+//profile/:userId --> GET = user
+app.get('/profile/:id', (req, res) => { profileID.handleProfileID(req, req, db) });
+
+//image --> PUT --> user
+app.put('/image', (req, res) => { image.handleImage(req, res, db) });
+app.post('/imageUrl', (req, res) => { image.handleApiCall(req, res) });
+
+
+app.listen(3000, ()=> {
+	console.log('app is running on port 3000');
+})
+
+
+
+
+/* Ideas how to set up that server
+/ ---> res = this is working
+/signin --> POST = success/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT --> user
+
+
+*/
